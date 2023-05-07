75ca3df1b651012fa5702914a8808beff3b82b9a
Truekexzh <132856054+Truekexzh@users.noreply.github.com>
2023-05-07 19:24:08 -0400

    Initial commit

---
+++ web.js
@@ -0,0 +1,11 @@
+servicios : 
+ vendedor : 
+  git_url : https : //github.com/cloud66 - samples/acme.git
+   git_branch : master
+   dockerfile_path :  "./Dockerfile" 
+  build_root : vendedor
+  comprador : 
+  git_url : https : //github.com/cloud66 - samples/ acme.git
+   git_branch : master
+   dockerfile_path :  "./Dockerfile" 
+  build_root : comprador
